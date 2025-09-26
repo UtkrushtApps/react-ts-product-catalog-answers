0fa111edc1446a0b825c9c2ebd97d68eddd0f9d5
Naman Muley <naman.g.muley@gmail.com>
2025-09-26 19:34:48 +0530

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ProductCatalog from './components/ProductCatalog';
+import './App.css';
+
+const App: React.FC = () => (
+  <div className="app">
+    <ProductCatalog />
+  </div>
+);
+
+export default App;
